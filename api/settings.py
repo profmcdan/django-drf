fc2d1f7372f65884bab6543a00103f79b7fa8a55
Daniel Ale <denielale9291@gmail.com>
2019-04-18 13:40:47 +0100

    Add models and serializer

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'courses',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ]
+}
---
+++ courses/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from . import models
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Review
+        extra_kwargs = {
+            "email": {"write_only": True}
+        }
+        fields = (
+            'id', 'course', 'name', 'email', 'review', 'rating', 'created_at'
+        )
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Course
+        fields = (
+            'id', 'title', 'url'
+        )
